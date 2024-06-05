7ccbcd3d9d725611fe9b982827897873f3f4439a
Mohit <oficial.mohit.uchit@gmail.com>
2024-06-06 02:08:26 +0530

    Remove submodule and add client files

--- server/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controller/auth/authController');
-
-router.post('/login', authController.login);
-
-router.post('/signup', authController.signup);
-
-module.exports = router;
--- server/src/services/logging/pinoService.js
+++
@@ -1,9 +0,0 @@
-const pino = require('pino');
-const logger = require('pino-http')({
-  logger: pino({
-    timestamp: pino.stdTimeFunctions.isoTime,
-  }),
-  autoLogging: false,
-});
-
-module.exports = logger;
